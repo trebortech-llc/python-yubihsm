e6ef0db2488782c7b127c5ad1c17ee95ddbf1608
Robert Booth <robert.booth@trebortech.com>
2019-07-17 13:22:58 -0500

    Add functions for Shamir's secret sharing

--- yubihsm/utils.py
+++ yubihsm/utils.py
@@ -15,13 +15,18 @@
 """Various utility functions used throughout the library."""
 
 
-from __future__ import absolute_import, division
+from __future__ import absolute_import, division, print_function
 
 import six
+import random
+import functools
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+_PRIME = 2**127 - 1
+# TODO: Change this to pull random number from YubiHSM
+_RINT = functools.partial(random.SystemRandom().randint, 0)
 
 def password_to_key(password):
     """Derive keys for establishing a YubiHSM session from a password.
@@ -40,3 +45,94 @@ def password_to_key(password):
     ).derive(password)
     key_enc, key_mac = key[:16], key[16:]
     return key_enc, key_mac
+
+
+def _eval_at(poly, x, prime):
+    '''evaluates polynomial (coefficient tuple) at x, used to generate a
+    shamir pool in make_random_shares below.
+    '''
+    accum = 0
+    for coeff in reversed(poly):
+        accum *= x
+        accum += coeff
+        accum %= prime
+    return accum
+
+
+def _extended_gcd(a, b):
+    '''
+    division in integers modulus p means finding the inverse of the
+    denominator modulo p and then multiplying the numerator by this
+    inverse (Note: inverse of A is B such that A*B % p == 1) this can
+    be computed via extended Euclidean algorithm
+    http://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Computation
+    '''
+    x = 0
+    last_x = 1
+    y = 1
+    last_y = 0
+    while b != 0:
+        quot = a // b
+        a, b = b, a%b
+        x, last_x = last_x - quot * x, x
+        y, last_y = last_y - quot * y, y
+    return last_x, last_y
+
+
+def _divmod(num, den, p):
+    '''compute num / den modulo prime p
+
+    To explain what this means, the return value will be such that
+    the following is true: den * _divmod(num, den, p) % p == num
+    '''
+    inv, _ = _extended_gcd(den, p)
+    return num * inv
+
+
+def _lagrange_interpolate(x, x_s, y_s, p):
+    '''
+    Find the y-value for the given x, given n (x, y) points;
+    k points will define a polynomial of up to kth order
+    '''
+    k = len(x_s)
+    assert k == len(set(x_s)), "points must be distinct"
+    def PI(vals):  # upper-case PI -- product of inputs
+        accum = 1
+        for v in vals:
+            accum *= v
+        return accum
+    nums = []  # avoid inexact division
+    dens = []
+    for i in range(k):
+        others = list(x_s)
+        cur = others.pop(i)
+        nums.append(PI(x - o for o in others))
+        dens.append(PI(cur - o for o in others))
+    den = PI(dens)
+    num = sum([_divmod(nums[i] * den * y_s[i] % p, dens[i], p)
+               for i in range(k)])
+    return (_divmod(num, den, p) + p) % p
+
+
+def make_random_shares(minimum, shares, prime=_PRIME):
+    '''
+    Generates a random shamir pool, returns the secret and the share
+    points.
+    '''
+    if minimum > shares:
+        raise ValueError("pool secret would be irrecoverable")
+    poly = [_RINT(prime) for i in range(minimum)]
+    points = [(i, _eval_at(poly, i, prime))
+              for i in range(1, shares + 1)]
+    return poly[0], points
+
+
+def recover_secret(shares, prime=_PRIME):
+    '''
+    Recover the secret from share points
+    (x,y points on the polynomial)
+    '''
+    if len(shares) < 2:
+        raise ValueError("need at least two shares")
+    x_s, y_s = zip(*shares)
+    return _lagrange_interpolate(0, x_s, y_s, prime)
